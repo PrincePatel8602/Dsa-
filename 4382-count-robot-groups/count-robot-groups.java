20dc44319202ff9dc79b432bbc42e60b53ac51f0
PrincePatel8602 <patelprince3698@gmail.com>
2026-09-11 20:42:13 +0530

    Time: 2 ms (100.00%) | Memory: 99.9 MB (60.95%) - LeetSync

--- 4382-count-robot-groups/count-robot-groups.java
+++ 4382-count-robot-groups/count-robot-groups.java
@@ -2,11 +2,7 @@ class Solution {
     public int countGroups(int[] position, int[] speed, int distance) {
         int  count =1;
         int minSped = speed[speed.length-1];
-        if(speed.length ==2){
-            if(speed[0]>speed[1] || position[1]-position[0] <= distance){
-                return 1;
-            }
-        }
+        
         for(int i=speed.length-1;i>=1;i--){
             if(minSped >=speed[i-1] && position[i]-position[i-1] > distance){       
                 minSped= speed[i-1];             
