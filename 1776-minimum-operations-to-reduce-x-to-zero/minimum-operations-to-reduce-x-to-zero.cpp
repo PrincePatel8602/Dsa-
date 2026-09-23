2be0e27332469f8d0b2fc83b5cc8eb3ac951912b
PrincePatel8602 <patelprince3698@gmail.com>
2026-09-23 17:34:51 +0530

    Time: 230 ms (7.68%) | Memory: 182.9 MB (5.01%) - LeetSync

--- 1776-minimum-operations-to-reduce-x-to-zero/minimum-operations-to-reduce-x-to-zero.cpp
+++ 1776-minimum-operations-to-reduce-x-to-zero/minimum-operations-to-reduce-x-to-zero.cpp
@@ -2,40 +2,33 @@ class Solution {
 public:
     int minOperations(vector<int>& nums, int x) {
         int n=nums.size();
-        unordered_map<int,int>pre;
+        vector<int>pre;
         unordered_map<int,int>suff;
-        int p=0;
-        int s=0;
+        int p=0, s=0;
+        int ans = INT_MAX;
         for(int i=0;i<n;i++){
             p+=nums[i];
-            pre[p]=i;
+            pre.push_back(p);
             s+=nums[n-1-i];
             suff[s]=i;
+            if(p==x || s==x){
+                ans = min(ans,i+1);
+            }
         }
-        int np=INT_MAX;
-        int cp=INT_MAX;
-        int ans=INT_MAX;
-        if(pre.find(x)!=pre.end()){
-            np=pre[x]+1;
+
+        for(int i=0; i<n; i++){
+            int req = x-pre[i];
+            if(suff.find(req) != suff.end()){
+                int op = i+1+suff[req]+1;
+                ans = min(ans,op);
+            }
         }
-        if(suff.find(x)!=suff.end()){
-            cp=suff[x]+1;
-        }
-        ans=min(np,cp);
-        
-        for(auto it:pre){
-            
-           if(suff.find(x-it.first)!=suff.end()){
-         
-                ans=min(ans,it.second+suff[x-it.first]+2); 
-                
-           } 
-        }
-        if(ans==INT_MAX){
+
+        if(ans == INT_MAX){
             return -1;
         }
         if(ans>n){
-           return -1;
+            return -1;
         }
         return ans;
     }
